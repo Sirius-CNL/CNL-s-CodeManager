ab22add7aa4af8f0b0e018e8487c4a5f71a1d3a9
LEGENDrd <16056152+legendrd@user.noreply.gitee.com>
2025-07-25 00:04:08 +0800

    Initial commit (purged history)

---
+++ README.md
@@ -0,0 +1,26 @@
+# CNL
+
+## Code Manager
+
+需要C++20
+
+由于使用了额外的库，请在编译时加入一下参数：
+
+```bash
+-std=c++20 -lstdc++fs -municode -Wall
+```
+
+或者使用已经编译好的文件：CM.exe
+
+请在系统环境变量`path`中添加这个文件所在的文件夹路径，这样就可以正常使用了。
+如果你的Windows版本较低，应该在`path`变量的末尾加上`;<这个文件夹的路径>`
+
+现有的命令：
+
+* add -  增加一道题目
+* list - 列出题目列表
+* open - 打开题目
+* help  - 帮助
+* about - 关于
+
+关注我 (LEGENDrd)：[Bilibili](https://space.bilibili.com/3546767803026063?spm_id_from=333.1007.0.0)，[CSDN博客](https://blog.csdn.net/R_D_LEGEND?type=blog)
---
+++ source/CodeManager.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+using namespace std::filesystem;
+
+// 获取codes目录路径
+path get_codes_dir()
+{
+    wchar_t exe_path[MAX_PATH];
+    GetModuleFileNameW(nullptr, exe_path, MAX_PATH);
+    path exe_dir = path(exe_path).parent_path();
+    return exe_dir / "codes";
+}
+
+// 确保codes目录存在
+void ensure_codes_dir_exists()
+{
+    path codes_dir = get_codes_dir();
+    if (!exists(codes_dir))
+    {
+        create_directory(codes_dir);
+    }
+}
+
+// 添加题目
+void add_problem(const wstring &name, const wstring &source_path)
+{
+    ensure_codes_dir_exists();
+    path codes_dir = get_codes_dir();
+    path dest_path = codes_dir / (name + L".cpp");
+
+    if (exists(dest_path))
+    {
+        wcerr << L"错误：已存在相同名称的题目，请更换名称\n";
+        return;
+    }
+
+    try
+    {
+        copy_file(source_path, dest_path);
+        wcout << L"题目 '" << name << L"' 添加成功\n";
+    }
+    catch (const filesystem_error &e)
+    {
+        wcerr << L"错误：无法添加题目 - " << e.what() << L"\n";
+    }
+}
+
+// 列出所有题目
+void list_problems()
+{
+    ensure_codes_dir_exists();
+    path codes_dir = get_codes_dir();
+
+    wcout << L"已有题目列表:\n";
+    try
+    {
+        for (const auto &entry : directory_iterator(codes_dir))
+        {
+            if (entry.is_regular_file() && entry.path().extension() == L".cpp")
+            {
+                wstring filename = entry.path().stem();
+                wcout << L"  - " << filename << L"\n";
+            }
+        }
+    }
+    catch (const filesystem_error &e)
+    {
+        wcerr << L"错误：无法读取题目列表 - " << e.what() << L"\n";
+    }
+}
+
+// 打开题目
+void start_problem(const wstring &name)
+{
+    ensure_codes_dir_exists();
+    path codes_dir = get_codes_dir();
+    path file_path = codes_dir / (name + L".cpp");
+
+    if (!exists(file_path))
+    {
+        wcerr << L"错误：题目 '" << name << L"' 不存在\n";
+        return;
+    }
+
+    wstring command = L"start \"\" \"" + file_path.wstring() + L"\"";
+    _wsystem(command.c_str());
+}
+
+// 显示帮助信息
+void show_help()
+{
+    wcout << L"CodeManager (CM) 使用说明:\n"
+          << L"> CM add <题目名称> <源文件路径>  - 添加题目\n"
+          << L"> CM list                         - 列出所有题目\n"
+          << L"> CM start <题目名称>             - 打开题目\n"
+          << L"> CM help                         - 显示帮助\n"
+          << L"   -------------by CNL 2025.7------------   \n";
+}
+
+void show_about()
+{
+    wcout << L"\n关于 CodeManager (CM) 1.0 \n"
+          << L"作者：CNL - LEGENDrd@163.com\n"
+          << L"这是我自己做的一个小程序，可能会有一些bug。\n"
+          << L"Gitee项目链接：https://gitee.com/legendrd/cnl\n"
+          << L"Bug提交：https://gitee.com/legendrd/cnl/issues（我能改就会改的）\n"
+          << L"关注 https://space.bilibili.com/3546767803026063?spm_id_from=333.1007.0.0\n"
+          << L"2025.7.24\n";
+
+}
+
+int wmain(int argc, wchar_t *argv[])
+{
+    setlocale(LC_ALL, "");
+    if (argc < 2)
+    {
+        show_help();
+        return 1;
+    }
+
+    wstring command = argv[1];
+
+    if (command == L"add" && argc == 4)
+    {
+        add_problem(argv[2], argv[3]);
+    }
+    else if (command == L"list" && argc == 2)
+    {
+        list_problems();
+    }
+    else if (command == L"start" && argc == 3)
+    {
+        start_problem(argv[2]);
+    }
+    else if (command == L"help" && argc == 2)
+    {
+        show_help();
+    }
+    else if (command == L"about")
+    {
+        show_about();
+    }
+    else
+    {
+        wcerr << L"错误：无效的命令或参数\n";
+        show_help();
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
